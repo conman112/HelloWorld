b0ac86a7fa887342a71b9769f62a039c3bef69ad
Cyrus Cordero <cyrus.cordero@gmail.com>
2016-01-19 10:57:28 -0500

    this is a test of how to do all this crap

--- myfirstpythonprogram.py
+++ myfirstpythonprogram.py
@@ -1,3 +1,13 @@
-print "hello world!"
+import pygame, sys
+from pygame.locals import *
 
-print "YO I JUS CHANGED SOM SHIT"
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('Hello World!')
+
+while True: # main game loop
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+	pygame.display.update()
--- mysecondpythonprogram.py
+++ mysecondpythonprogram.py
@@ -1,3 +1,18 @@
 print "YO LOLOLOLOL"
 
 print "MAKE ANOTHER EDIT"
+
+print "new thing"
+
+def main():
+	print "do the thing!"
+	x = 5
+	print x
+
+
+if __name__ == "__main__":
+	main()
+
+print "trying this shit out"
+
+print "this is another test"
